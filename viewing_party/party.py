e3a83eff5071fb8081adff6dedf967074d063493
Laura Castro <lauracastrotech@gmail.com>
2025-03-25 17:22:38 -0400

    completed Wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,7 +2,47 @@
 
 # something just for testing
 def create_movie(title, genre, rating):
-    pass
+    if not title or not genre or not rating:
+        return None
+    new_movie = {}
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    # add move to user data
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    watched_movie = user_data["watched"] # [{}, {}]
+    watchlist = user_data["watchlist"] # [{}, {}]
+    for movie in watchlist:
+        if movie["title"] == title:
+            watched_movie.append(movie)
+            watchlist.remove(movie)
+    return user_data
+
+
+
+'''
+- take two parameters: `user_data`, `title`
+  - the value of `user_data` will be a dictionary with a `"watchlist"` and a `"watched"`
+    - This represents that the user has a watchlist and a list of watched movies
+  - the value of `title` will be a string
+    - This represents the title of the movie the user has watched
+- If the title is in a movie in the user's watchlist:
+  - remove that movie from the watchlist
+  - add that movie to watched
+  - return the `user_data`
+- If the title is not a movie in the user's watchlist:
+  - return the `user_data`
+'''
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
